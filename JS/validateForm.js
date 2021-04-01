79ed2ab1e2effb1c01904709b30e7dc76c211066
Antonpupil <70876693+Antonpupil@users.noreply.github.com>
2021-04-01 20:22:00 +0300

    не полностью валидация формы

--- JS/validateForm.js
+++ JS/validateForm.js
@@ -1,31 +1,73 @@
 (function () {
-    const form = document.querySelector("#order-form");
+    const orderForm = document.querySelector("#order-form");
     const send = document.querySelector(".form__submit");
+    const phone = document.querySelector("#phone-number");
+    console.log(orderForm);
+    console.log(send);
 
-    send.addEventListener("click", (e)=>{
-        e.preventDefault();
+    phone.addEventListener("keydown", (e) => {
+        try {
+            console.log(e.key);
+            let isDidgit = false;
+            let isPlus = false;
+            let isDash = false;
+            let isAction = false;
+
+            if (e.key >= 0 || e.key <= 9) {
+                isDidgit = true;
+            };
+            if (e.key == "+") {
+                isPlus = true;
+            };
+            if (e.key == "-") {
+                isDash = true;
+            };
+            if (e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "Backspace") {
+                isAction = true;
+            };
+
+            if (!isDidgit && !isPlus && !isAction && !isDash) {
+                e.preventDefault();
+                throw new Error("Вводить можно только числа, +, -")
+            }
+            e.target.nextElementSibling.textContent = "";
+        } catch (error) {
+            e.target.nextElementSibling.textContent = error.message;
+            e.preventDefault();
+        };
 
     });
 
-    function validateForm(form){
+    send.addEventListener("click", (e) => {
+        e.preventDefault();
+    });
+
+    function validateForm(orderForm) {
+        console.log(orderForm);
         let valid = true;
 
-        if (!validate(form.elements.name)){
+        if (!validate(orderForm.elements.name)) {
+            valid = false;
+        }
+        if (!validate(orderForm.elements.phone)) {
+            valid = false;
+        }
+        if (!validate(orderForm.elements.comment)) {
             valid = false;
         }
         return valid;
-    }
+    };
 
-function validate(element){
-    if (!element.checkValidity()) {
-        element.nextElementSibling.textContent = element.validationMessage;
-        element.style.border = "1px solid red";
-        return false;
-    } else {
-        element.nextElementSibling.textContent = "";
-        element.style.border = "none";
-        return true;
-    }
-}
-
-}); 
+    function validate(element) {
+        if (!element.checkValidity()) {
+            element.value.trim();
+            element.nextElementSibling.textContent = element.validationMessage;
+            element.style.border = "1px solid red";
+            return false;
+        } else {
+            element.nextElementSibling.textContent = "";
+            element.style.border = "none";
+            return true;
+        }
+    };
+})();
